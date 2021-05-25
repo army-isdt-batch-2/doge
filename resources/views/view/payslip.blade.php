f8036397bd26d95637433c8349403f8c5512e764
jagdadler <agila244@gmail.com>
2021-05-25 14:39:32 +0800

    testing lang :)

--- resources/views/view/payslip.blade.php
+++ resources/views/view/payslip.blade.php
@@ -1,6 +1,6 @@
 @php $active = 'payslip' @endphp
 @extends('layouts.main')
-@section('title', 'View Payslip')
+@section('title', 'View payment')
 @section('content')
 <div class="row align-items-center">
     <div class="col-12 mt-5">
